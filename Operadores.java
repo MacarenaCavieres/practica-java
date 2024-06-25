82f258dc4a2279db5da97e75b6beb8e4f830c7d1
MCavieres <maca.c.91@gmail.com>
2024-06-25 16:59:14 -0400

    practica con lenguaje java

---
+++ Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+        double a = 5;
+        double b;
+
+        b = 7;
+
+        double c = b/a;
+        
+        System.out.println(c);
+
+        final double apulgadas = 2.54;
+        final double cm = 6;
+        final double resultado = cm / apulgadas;
+
+        System.out.println("En " + cm + " cm hay " + resultado + " pulgadas" );
+
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,6 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte edad = 18;
+        System.out.println(edad);
+    }
+}
