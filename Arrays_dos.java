122ffdfbb11d01991294d8d40621dce36a9875a0
MCavieres <maca.c.91@gmail.com>
2024-07-01 17:58:44 -0400

    practica de arrays

---
+++ Arrays_dos.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class Arrays_dos {
+    public static void main(String[] args) {
+        // String paises[] = { "Chile", "Jamaica", "Canada", "Australlia", "Peru",
+        // "Brasil", "Costa Rica", "Mexico" };
+
+        // String[] paises = new String[8];
+        // for (int i = 0; i < 8; i++) {
+        // paises[i] = JOptionPane.showInputDialog("Ingresa un pais " + (i + 1));
+        // }
+
+        // for (String item : paises) {
+        // System.out.println(item);
+        // }
+
+        int[] aleatorios = new int[150];
+
+        for (int i = 0; i < aleatorios.length; i++) {
+            aleatorios[i] = (int) Math.round(Math.random() * 100);
+        }
+
+        for (int num : aleatorios) {
+            System.out.print(num + " - ");
+        }
+
+    }
+}
---
+++ Arrays_uno.java
@@ -0,0 +1,9 @@
+public class Arrays_uno {
+    public static void main(String[] args) {
+        int mi_array[] = { 2, 7, 2, 8, 5 };
+
+        for (int i = 0; i < mi_array.length; i++) {
+            System.out.println(mi_array[i]);
+        }
+    }
+}
