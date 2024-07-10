06337b7354b98ed7af3c20feda127db1564f1e7b
MCavieres <maca.c.91@gmail.com>
2024-07-10 16:44:47 -0400

    objetos

---
+++ poo/Clase2.java
@@ -0,0 +1,12 @@
+package poo;
+
+import paquetepruebas.*;
+
+public class Clase2 {
+    public static void main(String[] args) {
+        Clase1 miobj = new Clase1();
+
+        Clase3 miobj2 = new Clase3();
+
+    }
+}
